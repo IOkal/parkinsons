4986dc9a482691adb761989cd9032cabf2ee1bb1
Tobias Carryer <TobiasC@Tobiass-MacBook-Air.local>
2019-11-23 12:47:40 -0500

    Add function to calculate fundamental frequency features

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,8 @@
 import os
 import json
 from tensorflow.keras.models import load_model
+from fundamental_frequency import calculate_fundamental_frequency_features
+import numpy as np
 from flask import Flask, request
 from google.cloud import storage
 
@@ -23,6 +25,20 @@ def _load_model():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    X = request.get_json()['X']
-    y = MODEL.predict(X).tolist()
+    # Get the path to the sound file from the request
+    wave_file_path = request.get_json()['wave_file_path']
+
+    # Get the sound file from Google Cloud Storage
+    sound_file = None  # Could use scipy.io.wavfile.read(wave_file_path)
+
+    # Calculate features
+    fundamental_frequency_features = calculate_fundamental_frequency_features(sound_file)
+
+    # Concatenate features in the order the model expects, then make a prediction.
+    model_input = np.concatenate(fundamental_frequency_features)
+    prediction_array = MODEL.predict(model_input)
+
+    # We only process one sound file so there should only be one prediction to return.
+    prediction = prediction_array[0]
+
     return json.dumps({'y': y}), 200
