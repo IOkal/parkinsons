09a61f317e54b8ddd32822187090caa57412b9c1
will31415 <william.ottawa@yahoo.com>
2019-11-23 08:24:28 -0500

    Not working
    Many configuration bugs when using Christian Library functions - will try alternative route

--- feature-engineering.py
+++ feature-engineering.py
@@ -5,3 +5,7 @@ sys.path.append(modulePath)
 import generalUtility
 import dspUtil
 import matplotlibUtil
+import praatUtil
+import os
+
+praatUtil.calculateF0("audio-file.wav", readProgress=0.01, acFreqMin=60, voicingThreshold=0.45, veryAccurate=False, fMax=2000, octaveJumpCost=0.35, silenceThreshold=0.03, octaveCost=0.01, voicedUnvoicedCost=0.14, maxNumCandidates=15, verbose=False)
