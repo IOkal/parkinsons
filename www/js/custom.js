a12f57324aa54fc918542d5d29f86ef5fff06c89
Tobias Carryer <TobiasC@Tobiass-MacBook-Air.local>
2019-11-24 06:48:44 -0500

    Format response to display in graph and probability

--- www/js/custom.js
+++ www/js/custom.js
@@ -1,37 +1,6 @@
 //-------- inference the neural network --------//
 
-const readUploadedFileAsText = inputFile => {
-  const temporaryFileReader = new FileReader();
 
-  return new Promise((resolve, reject) => {
-    temporaryFileReader.onerror = () => {
-      temporaryFileReader.abort();
-      reject(new DOMException("Problem parsing input file."));
-    };
-
-    temporaryFileReader.onload = () => {
-      console.log(temporaryFileReader);
-      resolve(temporaryFileReader.result);
-    };
-    temporaryFileReader.readAsBinaryString(inputFile);
-  });
-};
-
-async function inference(file) {
-  console.log(file);
-  console.log("Entering inference function");
-  var data = await readUploadedFileAsText(file);
-  console.log(data);
-  var response = await lib.tobcar["parkinsons-classification"]["@dev"]({
-    file: data
-  });
-
-  document.getElementById("prediction").value = response.prediction;
-  document.getElementById("averageFundamentalFrequency").value = response.averageFundamentalFrequency;
-  document.getElementById("jitter").value = response.jitter;
-  document.getElementById("shimmer").value = response.shimmer;
-  console.log(response);
-}
 
 function handleFiles(files) {
   files = [...files];
